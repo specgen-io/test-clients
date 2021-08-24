15a1383209040718369006baa937e8e1924ca50c
Anastasiya <nislovskaya96@gmail.com>
2021-08-24 22:39:25 +0300

    service-go: Updated test-service

--- test-service/v2/echo_service.go
+++
@@ -1,9 +0,0 @@
-package v2
-
-import "test-service/spec/v2"
-
-type EchoService struct{}
-
-func (service *EchoService) EchoBody(body *v2.Message) (*v2.EchoBodyResponse, error) {
-	return &v2.EchoBodyResponse{Ok: body}, nil
-}
