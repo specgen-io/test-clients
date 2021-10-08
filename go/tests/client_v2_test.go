c835a23d29639282cf9902ec3b273674172cb51f
Vladimir Sapronov <vladimir@specgen.io>
2021-10-08 00:43:25 -0400

    go: Fix responses conflict

--- go/tests/client_v2_test.go
+++ go/tests/client_v2_test.go
@@ -2,13 +2,13 @@ package tests
 
 import (
 	"gotest.tools/assert"
-	spec "test-client/spec/v2"
+	"test-client/spec/v2/echo"
 	"test-client/spec/v2/models"
 	"testing"
 )
 
 func Test_Echo_Body_V2(t *testing.T) {
-	client := spec.NewEchoClient(service_url)
+	client := echo.NewClient(service_url)
 	expectedMessage := &models.Message{true, "the string"}
 	response, err := client.EchoBody(&models.Message{true, "the string"})
 	assert.NilError(t, err)
