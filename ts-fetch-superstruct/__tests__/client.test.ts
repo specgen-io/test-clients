bba227267ec32b162fce498b8ada37eed18f92a5
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-12-01 14:14:34 -0500

    ts-fetch-superstruct: Added initial version

---
+++ ts-fetch-superstruct/__tests__/client.test.ts
@@ -0,0 +1,52 @@
+import {Message, Choice} from '../test-service/models'
+import {client as echoClient} from '../test-service/echo'
+//import {client as checkClient} from '../test-service/check'
+
+const config = {baseURL: process.env.SERVICE_URL!}
+
+describe('client echo', function() {
+    let client = echoClient(config)
+    it('echoBody', async function() {
+        let body: Message = {int_field: 123, string_field: "the string"}
+        let response = await client.echoBody({body})
+        expect(response).toStrictEqual(body);
+    })
+
+    it('echoQuery', async function() {
+        let expected: Message = {int_field: 123, string_field: "the string"}
+        let response = await client.echoQuery({intQuery: 123, stringQuery: "the string"})
+        expect(response).toStrictEqual(expected);
+    })
+
+    it('echoHeader', async function() {
+        let expected: Message = {int_field: 123, string_field: "the string"}
+        let response = await client.echoHeader({intHeader: 123, stringHeader: "the string"})
+        expect(response).toStrictEqual(expected);
+    })
+
+    it('echoUrlParams', async function() {
+        let expected: Message = {int_field: 123, string_field: "the string"}
+        let response = await client.echoUrlParams({intUrl: 123, stringUrl: "the string"})
+        expect(response).toStrictEqual(expected);
+    })
+});
+
+// describe('client check', function() {
+//     let client = checkClient(config)
+//     it('checkQuery', async function() {
+//         let body: Message = {int_field: 123, string_field: "the string"}
+//         await client.checkQuery({
+//             pString: "the string",
+//             pStringArray: ["string 1", "string 2"],
+//             pDate: "2021-01-01",
+//             pDateArray: ["2021-01-02"],
+//             pDatetime: new Date("2021-01-02T23:54"),
+//             pInt: 123,
+//             pLong: 123,
+//             pDecimal: 123,
+//             pEnum: Choice.SECOND_CHOICE,
+//             pStringOpt: "some string",
+//             pStringDefaulted: "the string",
+//         })
+//     })
+// });
