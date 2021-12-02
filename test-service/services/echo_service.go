ada02d01fa3595d477eb985da968c5e6dac5b8fc
Nislovskaya96 <nislovskaya96@gmail.com>
2021-12-02 18:07:53 +0300

    gengo: Fixed services implementations

--- test-service/services/echo_service.go
+++ test-service/services/echo_service.go
@@ -1,14 +1,15 @@
 package services
 
-import "test-service/spec/echo"
-import "test-service/spec/models"
+import (
+	"test-service/spec/echo"
+	"test-service/spec/models"
+)
 
 type EchoService struct{}
 
-func (service *EchoService) EchoBodyString(body *string) (*string, error) {
-	return body, nil
+func (service *EchoService) EchoBodyString(body string) (*string, error) {
+	return &body, nil
 }
-
 func (service *EchoService) EchoBody(body *models.Message) (*models.Message, error) {
 	return body, nil
 }
@@ -21,7 +22,6 @@ func (service *EchoService) EchoHeader(intHeader int, stringHeader string) (*mod
 func (service *EchoService) EchoUrlParams(intUrl int, stringUrl string) (*models.Message, error) {
 	return &models.Message{IntField: intUrl, StringField: stringUrl}, nil
 }
-
 func (service *EchoService) SameOperationName() (*echo.SameOperationNameResponse, error) {
 	return &echo.SameOperationNameResponse{Ok: &echo.Empty}, nil
 }
