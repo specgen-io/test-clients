6bd8f2d4cdf47029076f7cf9b969ffbb4104a331
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-12-06 19:17:08 -0500

    ts-fetch-superstruct: Testing params stringify

--- ts-fetch-superstruct/__tests__/client.test.ts
+++ ts-fetch-superstruct/__tests__/client.test.ts
@@ -36,22 +36,22 @@ test('echoUrlParams', async function() {
   assert.equal(response, expected, 'response matches expected')
 })
 
-// test('checkQuery', async function() {
-//   let client = checkClient(config)
-//   let body: Message = {int_field: 123, string_field: "the string"}
-//     await client.checkQuery({
-//         pString: "the string",
-//         pStringArray: ["string 1", "string 2"],
-//         pDate: "2021-01-01",
-//         pDateArray: ["2021-01-02"],
-//         pDatetime: new Date("2021-01-02T23:54"),
-//         pInt: 123,
-//         pLong: 123,
-//         pDecimal: 123,
-//         pEnum: Choice.SECOND_CHOICE,
-//         pStringOpt: "some string",
-//         pStringDefaulted: "the string",
-//     })
-// })
+test('checkQuery', async function() {
+  let client = checkClient(config)
+  let body: Message = {int_field: 123, string_field: "the string"}
+    await client.checkQuery({
+        pString: "the string",
+        pStringArray: ["string 1", "string 2"],
+        pDate: "2021-01-01",
+        pDateArray: ["2021-01-02"],
+        pDatetime: new Date("2021-01-02T23:54"),
+        pInt: 123,
+        pLong: 123,
+        pDecimal: 123,
+        pEnum: Choice.SECOND_CHOICE,
+        pStringOpt: "some string",
+        pStringDefaulted: "the string",
+    })
+})
 
 test.run();
