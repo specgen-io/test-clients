37b9bf0d72b9a2c5202a3d616ce314a24f1a321a
Anastasiya Nislovskaya <anastasiya@specgen.io>
2022-06-14 19:29:55 +0300

    java/kotlin_arrays: Fixed tests (#27)
    * java/kotlin_arrays: Fixed tests
    
    * Upgraded specgen version

--- kotlin-okhttp-jackson/build.gradle.kts
+++ kotlin-okhttp-jackson/build.gradle.kts
@@ -14,12 +14,12 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.assertj:assertj-core:3.21.0")
+    testImplementation("org.assertj:assertj-core:3.23.1")
 }
 
 specgen {
